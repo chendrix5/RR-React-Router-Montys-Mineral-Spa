1f5ddd8c75c860f6410c7091be85a3f1f3513c67
clay hendrix <clayhendrix5@gmail.com>
2022-11-21 20:33:03 -0600

    yo

--- react-router/src/components/Packages.js
+++ react-router/src/components/Packages.js
@@ -1,12 +1,22 @@
 
 
-export default function Packages() {
+export default function Packages(props) {
+    const displayPackages = props.Packages.map((eachPackage) => {
+      return (
+       <li>{eachPackage}</li> 
+       )
+    })
 
     return (
         <div>
             <div className="packages">
                 <div className="packagesHeader">
                     <h1>Our Packages</h1>
+                </div>
+                <div>
+                    <ul>
+                        {displayPackages}
+                    </ul>
                 </div>
                 
             </div>
